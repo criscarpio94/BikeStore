1a89bcf561876c6102dc92ad7bb9faf0f89ed598
Jonny <jonnycampuzano37@gmail.com>
2026-08-22 15:40:42 -0500

    Ajuste controller de categorias

--- BikeStore.Api/Controllers/CategoriasController.cs
+++ BikeStore.Api/Controllers/CategoriasController.cs
@@ -1,6 +1,58 @@
-﻿namespace BikeStore.Api.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BikeStore.Api.Data;
+using BikeStore.Api.Models;
+
+namespace BikeStore.Api.Controllers
 {
-    public class CategoriasController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetCategorias() =>
+            await _context.Categoria.ToListAsync();
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Categoria>> GetCategoria(int id)
+        {
+            var categoria = await _context.Categoria.FindAsync(id);
+            return categoria == null ? NotFound() : categoria;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Categoria>> PostCategoria(Categoria categoria)
+        {
+            _context.Categoria.Add(categoria);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetCategoria), new { id = categoria.IdCategoria }, categoria);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategoria(int id, Categoria categoria)
+        {
+            if (id != categoria.IdCategoria) return BadRequest();
+            _context.Entry(categoria).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategoria(int id)
+        {
+            var categoria = await _context.Categoria.FindAsync(id);
+            if (categoria == null) return NotFound();
+            _context.Categoria.Remove(categoria);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
     }
 }
