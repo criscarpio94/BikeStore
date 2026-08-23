fb5cb91f7de7efefac98827d8661bb5e8e79ff9a
isaacmorales200016-celda <isaacmorales200016@gmail.com>
2026-08-23 09:47:20 -0500

    Modelos para web de cliente y bicicleta

---
+++ BikeStore.Web/Models/BicicletaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BikeStore.Web.Models
+{
+    public class BicicletaViewModel
+    {
+        public int IdBicicleta {  get; set; }
+        public int IdCategoria { get; set; }        
+        public string Marca { get; set; } = string.Empty;
+        public string Modelo { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public string Estado { get; set; } = "Disponible";
+        public string? CategoriaNombre { get; set; }
+        public List<SelectListItem> CategoriasList { get; set; } = new();
+
+    }
+}
---
+++ BikeStore.Web/Models/ClienteViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BikeStore.Web.Models
+{
+    public class ClienteViewModel
+    {
+        public int IdCliente { get; set; }
+        public string Cedula { get; set; } = string.Empty;
+        public string Nombres {  get; set; } = string.Empty;
+        public string Apellidos {  get; set; } = string.Empty;
+        public string? Telefono { get; set; }
+        public string? Correo { get; set; }
+
+    }
+}
