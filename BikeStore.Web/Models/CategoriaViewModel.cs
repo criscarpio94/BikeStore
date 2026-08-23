f32ecca6737520479663952c9550eea847d08858
isaacmorales200016-celda <isaacmorales200016@gmail.com>
2026-08-23 09:21:45 -0500

    Modelo para categoria

---
+++ BikeStore.Web/Models/CategoriaViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BikeStore.Web.Models
+{
+    public class CategoriaViewModel
+    {
+        public int IdCategoria { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string? Descripcion {  get; set; }
+        public bool Activo { get; set; } = true;
+    }
+}
